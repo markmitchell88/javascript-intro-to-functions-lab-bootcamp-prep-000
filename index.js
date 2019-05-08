b7b33e0ede54c5238e824dfcb32189e37e0cae30
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-08 09:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase){
   return "I can\'t hear you!"
 }
-elseif (string === string.toUpperCase){
+else if {
+} (string === string.toUpperCase){
   return "YES INDEED!"
 }
 else (string === "I love you, Grandma"){
